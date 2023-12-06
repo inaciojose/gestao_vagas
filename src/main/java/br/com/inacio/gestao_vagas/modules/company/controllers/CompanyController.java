57274cddab1b576edcd0c1789802f647cf12a2a0
inaciojose <inacioj086@gmail.com>
2023-12-06 10:16:18 -0300

    creating method to access use case create company

--- src/main/java/br/com/inacio/gestao_vagas/modules/company/controllers/CompanyController.java
+++ src/main/java/br/com/inacio/gestao_vagas/modules/company/controllers/CompanyController.java
@@ -1,10 +1,32 @@
 package br.com.inacio.gestao_vagas.modules.company.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.inacio.gestao_vagas.exceptions.UserFoundExcpetion;
+import br.com.inacio.gestao_vagas.modules.company.entities.CompanyEntity;
+import br.com.inacio.gestao_vagas.modules.company.useCases.CreateCompanyUseCase;
 
 @RestController
 @RequestMapping("/company")
 public class CompanyController {
-  
+
+  @Autowired
+  private CreateCompanyUseCase createCompanyUseCase;
+
+  @PostMapping("/")
+  public ResponseEntity<Object> create(@RequestBody CompanyEntity companyEntity) {
+    try {
+      var result = this.createCompanyUseCase.execute(companyEntity);
+      return ResponseEntity.ok().body(result);
+    } catch (UserFoundExcpetion e) {
+      return ResponseEntity.badRequest().body(e.getMessage());
+    } catch (Exception e) {
+      return ResponseEntity.badRequest().body(e.getMessage());
+    }
+  }
 }
