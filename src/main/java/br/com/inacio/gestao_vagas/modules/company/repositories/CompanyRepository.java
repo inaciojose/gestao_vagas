dc7c3a11b18755b77c671453cf8b6a71163ed3ff
inaciojose <inacioj086@gmail.com>
2023-12-06 09:57:54 -0300

    Adding validation before create a new company

--- src/main/java/br/com/inacio/gestao_vagas/modules/company/repositories/CompanyRepository.java
+++ src/main/java/br/com/inacio/gestao_vagas/modules/company/repositories/CompanyRepository.java
@@ -1,5 +1,12 @@
 package br.com.inacio.gestao_vagas.modules.company.repositories;
 
-public interface CompanyRepository {
+import java.util.Optional;
+import java.util.UUID;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.inacio.gestao_vagas.modules.company.entities.CompanyEntity;
+
+public interface CompanyRepository extends JpaRepository<CompanyEntity, UUID> {
+  Optional<CompanyEntity> findByUsernameOrEmail(String username, String email);
 }
--- src/main/java/br/com/inacio/gestao_vagas/modules/company/useCases/CreateCompanyUseCase.java
+++ src/main/java/br/com/inacio/gestao_vagas/modules/company/useCases/CreateCompanyUseCase.java
@@ -1,5 +1,25 @@
 package br.com.inacio.gestao_vagas.modules.company.useCases;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.inacio.gestao_vagas.exceptions.UserFoundExcpetion;
+import br.com.inacio.gestao_vagas.modules.company.entities.CompanyEntity;
+import br.com.inacio.gestao_vagas.modules.company.repositories.CompanyRepository;
+
+@Service
 public class CreateCompanyUseCase {
 
+  @Autowired
+  private CompanyRepository companyRepository;
+
+  public CompanyEntity execute(CompanyEntity companyEntity) {
+
+    this.companyRepository.findByUsernameOrEmail(companyEntity.getUsername(), companyEntity.getEmail())
+        .ifPresent((user) -> {
+          throw new UserFoundExcpetion();
+        });
+
+    return this.companyRepository.save(companyEntity);
+  }
 }
