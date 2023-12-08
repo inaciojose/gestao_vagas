eedd20823f5de4b5def0d719a10f9cdeadcbed29
inaciojose <inacioj086@gmail.com>
2023-12-08 09:51:33 -0300

    Creating new class "JobController"

---
+++ src/main/java/br/com/inacio/gestao_vagas/modules/company/controllers/JobController.java
@@ -0,0 +1,8 @@
+package br.com.inacio.gestao_vagas.modules.company.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class JobController {
+
+}
