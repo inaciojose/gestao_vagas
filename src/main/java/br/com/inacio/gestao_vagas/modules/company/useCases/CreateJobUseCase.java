675a5377eef9e57e4606f0874d6b49725268a2e8
inaciojose <inacioj086@gmail.com>
2023-12-08 09:36:37 -0300

    Adding method

--- src/main/java/br/com/inacio/gestao_vagas/modules/company/useCases/CreateJobUseCase.java
+++ src/main/java/br/com/inacio/gestao_vagas/modules/company/useCases/CreateJobUseCase.java
@@ -1,5 +1,14 @@
 package br.com.inacio.gestao_vagas.modules.company.useCases;
 
+import org.springframework.stereotype.Service;
+
+import br.com.inacio.gestao_vagas.modules.company.entities.JobEntity;
+
+@Service
 public class CreateJobUseCase {
-  
+
+  public void execute(JobEntity jobEntity) {
+    
+  }
+
 }
