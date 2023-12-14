c808e550a282372ca5c936a57aac44de2068aae3
inaciojose <inacioj086@gmail.com>
2023-12-14 09:58:10 -0300

    add security configuration(disable csrf)

--- src/main/java/br/com/inacio/gestao_vagas/security/SecurityConfig.java
+++ src/main/java/br/com/inacio/gestao_vagas/security/SecurityConfig.java
@@ -1,8 +1,15 @@
 package br.com.inacio.gestao_vagas.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
-  
+
+  SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http.csrf(csfr -> csfr.disable());
+    return http.build();
+  }
+
 }
