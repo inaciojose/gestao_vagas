00990e13b72492985f8fe5fcef427772a966a841
inaciojose <inacioj086@gmail.com>
2023-12-14 09:59:30 -0300

    add annotation

--- src/main/java/br/com/inacio/gestao_vagas/security/SecurityConfig.java
+++ src/main/java/br/com/inacio/gestao_vagas/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package br.com.inacio.gestao_vagas.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -7,6 +8,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig {
 
+  @Bean
   SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http.csrf(csfr -> csfr.disable());
     return http.build();
