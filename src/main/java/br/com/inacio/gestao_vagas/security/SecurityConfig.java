18cc3ccb4372abdb2430914909b179e6e88e3d2e
inaciojose <inacioj086@gmail.com>
2024-01-03 08:54:06 -0300

    Adicionando autenticação para acesso a rota

--- src/main/java/br/com/inacio/gestao_vagas/security/SecurityConfig.java
+++ src/main/java/br/com/inacio/gestao_vagas/security/SecurityConfig.java
@@ -10,7 +10,12 @@ public class SecurityConfig {
 
   @Bean
   SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    http.csrf(csfr -> csfr.disable());
+    http.csrf(csfr -> csfr.disable())
+        .authorizeHttpRequests(auth -> {
+          auth.requestMatchers("/candidate/").permitAll()
+              .requestMatchers("/company/").permitAll();
+          auth.anyRequest().authenticated();
+        });
     return http.build();
   }
 
